eeb5ad866003e9378dba19e66f8231b33ba9f043
epode4 <epode4@gamil.com>
2023-08-31 23:05:09 +0900

    update

---
+++ programmers/가장큰수찾기/sol.py
@@ -0,0 +1,6 @@
+def solution(array):
+    answer = [max(array),array.index(max(array))]
+    return answer
+
+print(solution([1, 8, 3]))
+print(solution([9, 10, 11, 8]))
---
+++ programmers/문자열정렬하기_1/sol.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    answer = []
+    for i in my_string:
+        if i.isdigit():
+            answer.append(int(i))
+
+    answer = sorted(answer)
+    return answer
+
+print(solution("hi12392"))
+print(solution("abcde0"))
