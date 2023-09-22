0a7e3d23c98bbda731b5dec507a4ad078583b345
Choi Sea In <140361644+epode4@users.noreply.github.com>
2023-09-22 21:06:24 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 98.11 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -3,9 +3,9 @@ def solution(s):
     stack = []
     for text in s:
         if text not in stack:
-            answer.append(-1)
             stack.append(text)
-        else:
+            answer.append(-1)
+        else:            
             count = 1
             for i in stack[::-1]:
                 if i != text:
@@ -14,4 +14,7 @@ def solution(s):
                     break
             answer.append(count)
             stack.append(text)
+            # num_list = [idx for idx, t in enumerate(stack) if t == text]
+            # answer.append(num_list[-1]-num_list[-2])
+
     return answer
