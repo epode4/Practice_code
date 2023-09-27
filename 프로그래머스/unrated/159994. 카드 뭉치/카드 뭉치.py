90cae45e955a58825af57ead0afa9e9739742ff9
Choi Sea In <140361644+epode4@users.noreply.github.com>
2023-09-27 17:43:56 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,24 @@
+def solution(cards1, cards2, goal):
+    answer = 'Yes'
+    card1 = []
+    card2 = []
+    for i in goal:
+        if i in cards1:
+            card1.append(cards1.index(i))
+        else:
+            card2.append(cards2.index(i))
+            
+    card_m = []
+    for a in range(len(card1)-1):
+        card_m.append(card1[a+1]-card1[a])
+    
+    for b in range(len(card2)-1):
+        card_m.append(card2[b+1]-card2[b])
+        
+    
+    for j in card_m:
+        if j < 0 or j > 1:
+            answer = 'No'
+
+        
+    return answer
