f7921c6e9577f47f586f36ed8e6d1e72f196c640
Choi Sea In <140361644+epode4@users.noreply.github.com>
2023-09-29 00:09:07 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,21 +1,16 @@
 def solution(N, stages):
     answer = []
     # 각 스테이지별 도전자 수와 실패자 수 구하기
+    # range error 피하기 위해 N+1까지
     stage_user = [0]*(N+1)
     stage_fail = [0]*(N+1)
-    # for idx, stage in enumerate(range(1,N+1)):
-    #     for i in stages:
-    #         if i >= stage:
-    #             stage_user[idx] += 1
-    #         if i == stage:
-    #             stage_fail[idx] += 1
     
     for stage in stages:
         for i in range(stage):
             stage_user[i] += 1
+            
         if stage <= N:
             stage_fail[stage-1] += 1
-    # print(stage_user, stage_fail)
         
     fail_count = []
     for user, fail in zip(stage_user, stage_fail):
