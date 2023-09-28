d4b92d8b3bd8788c6f6dbdd257cb3c73f5feccec
Choi Sea In <140361644+epode4@users.noreply.github.com>
2023-09-28 17:13:38 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/README.md
+++ 프로그래머스/lv1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.44 ms
+메모리: 10.2 MB, 시간: 0.24 ms
 
 ### 구분
 
--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,22 +1,7 @@
-# def solution(nums):
-#     pocket = []
-#     num = len(nums)/2
-#     # print(num)
-#     while nums and len(pocket) < num:
-#         for i in nums:
-#             if i not in pocket:
-#                 pocket.append(i)
-#                 nums.remove(i)
-#             else:
-#                 nums.remove(i)
-#     return len(pocket)
-
 def solution(nums):
-    nums_2 = set(nums)
+    nums_set = set(nums)
     pocket = []
-    # print(num)
-    for i in nums_2:
+    for i in nums_set:
         if len(pocket) < (len(nums)/2):
             pocket.append(i)
-        # print(pocket)
     return len(pocket)
