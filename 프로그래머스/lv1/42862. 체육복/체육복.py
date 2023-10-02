20c5e146c3fa950aaf5a3d68ef1ff7bcf1480458
Choi Sea In <140361644+epode4@users.noreply.github.com>
2023-10-02 14:05:44 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -7,20 +7,16 @@ def solution(n, lost, reserve):
     for i in reserve:
         result[i-1] += 1
     
-    # print(result)
-    
+    # 맨 앞 2 케이스만 따로 계산
     if result[0] == -1 and result[1] == 1:
         result[0] += 1
         result[1] -= 1
-    if result[-1] == -1 and result[-2] == 1:
-        result[-1] += 1
-        result[-2] -= 1
+        
     if result[0] == 1 and result[1] == -1:
         result[0] -= 1
         result[1] += 1
-     
-    # print(result)
-        
+
+    # 나머지 index 1부터 마지막 전까지 다음 것과 비교
     for i in range(1, len(result)-1):
         if result[i] == -1 and result[i+1] == 1:
             result[i] += 1
@@ -30,29 +26,5 @@ def solution(n, lost, reserve):
             result[i+1] += 1          
         
     
-    
-    # print(result)
-    
     return len(list(x for x in result if x >= 0))
     
-# def solution(n, lost, reserve):
-    
-#     lost.sort()
-#     reserve.sort()
-    
-#     not_lost = []
-    
-#     for i in range(n):
-#         if i+1 not in lost:
-#             not_lost.append(i+1)        
-    
-#     result = []
-#     for i in lost:
-#         for j in reserve:
-#             if i == j:
-#                 break
-#             if i + 1 == j:
-#                 result.append(i)
-#                 result.append(j)
-                
-#     return len(set(not_lost+result))
